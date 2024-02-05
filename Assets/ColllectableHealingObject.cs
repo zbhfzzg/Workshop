2d24f5fbbbc89c97afed06b11aa8729d92377bd2
Bohan Zhang <zhanghanfzzg@gmail.com>
2024-02-05 13:07:17 +0800

    workshop3

---
+++ Assets/ColllectableHealingObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColllectableHealingObject : MonoBehaviour
+{
+    public int healAmount = 5;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            RubyController controller = collision.GetComponent<RubyController>();
+            if (controller.HP < controller.maxHP)
+            {
+                print("Player HP is now " + controller.currentHP);
+                controller.ChangeHP(healAmount);
+                Destroy(this.gameObject);
+            }
+            
+        }
+    }
+}
--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,22 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RubyController : MonoBehaviour
 {
-    [Range(0,10f)]
+    [Range(0, 10f)]
     public float speed;
+    public Rigidbody2D rb;
+    private float horizontal;
+    private float vertical;
+    public int HP;
+    public int currentHP { get => HP; }
+
+    public int maxHP;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float X_movement = Input.GetAxis("Horizontal");
-        float Y_movement = Input.GetAxis("Vertical");
-        transform.position += (new Vector3(X_movement, Y_movement,0)) * speed*Time.deltaTime;
-        Debug.Log("X_movement : " + X_movement + "Y_movement : " + Y_movement);
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = transform.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        rb.MovePosition(position);
+    }
+
+    public void ChangeHP(int value)
+    {
+        HP += value;
+        if(HP > maxHP)
+        {
+            HP = maxHP;
+        }
+        if (HP < 0)
+        {
+            HP = 0;
+        }
+
+        HP = Mathf.Clamp(HP, 0, maxHP);
+        Debug.Log("Player hp is now" + HP);
     }
 }
